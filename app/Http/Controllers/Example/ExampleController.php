de16ea66d8dddc6ef4edc80156eb9bd736eb4847
Jpetchan <33972755+Jpetchan@users.noreply.github.com>
2017-12-28 03:38:15 +0000

    Initial commit

---
+++ app/Http/Controllers/Example/ExampleController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Example;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class ExampleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('Example.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('welcome');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return view('welcome');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view('welcome');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('welcome');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return view('welcome');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return view('welcome');
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>AliBoxStudio Management</title>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin-top: -200px;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            
+            #btnlogin {
+            position:relative;
+            margin-top:10%;
+            background:#fff;
+            border:3px solid #055388;
+            padding:20px;
+            font-size:0.9em;
+            color:white;
+            
+            font-family: 'Open Sans', sans-serif;
+            color: #055388;
+            font-weight:600;
+            letter-spacing:5px;
+            text-transform:uppercase;
+            border-radius: 5px;
+            
+            }
+
+            #btnlogin:hover {
+            bottom:4px;
+            box-shadow:0px 2px 0px 0px #055388;
+            transition: all .01s ease-in-out;
+            -moz-transition: all .01s ease-in-out;
+            -webkit-transition: all .01s ease-in-out;
+            }
+
+            #btnlogin:active {
+            top:4px;
+            box-shadow:inset 0px 2px 0px 0px #055388;
+            transition: all .01s ease-in-out;
+            -moz-transition: all .01s ease-in-out;
+            -webkit-transition: all .01s ease-in-out;
+            
+            /*bottom:0px;
+            box-shadow:0px 0px 0px 0px #055388;
+            transition: all .01s ease-in-out;
+            -moz-transition: all .01s ease-in-out;
+            -webkit-transition: all .01s ease-in-out;*/
+            
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <img src="./img/blogo.png" alt="Fjords" width="300" height="auto">
+                <div class="title">AliBoxStudio Management</div>
+                <button id="btnlogin">LOG IN</button></br>
+            </div>
+        </div>
+    </body>
+</html>
